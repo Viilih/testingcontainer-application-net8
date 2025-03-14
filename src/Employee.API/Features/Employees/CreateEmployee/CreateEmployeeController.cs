2fc462ca2c34d35ad926b793d6bccb259c2e73e7
vili <gnlnascimento@gmail.com>
2025-03-14 17:51:23 -0300

    feat: update controller routes

--- src/Employee.API/Features/Employees/CreateEmployee/CreateEmployeeController.cs
+++ src/Employee.API/Features/Employees/CreateEmployee/CreateEmployeeController.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace Employee.API.Features.Employees.CreateEmployee;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api")]
 public class CreateEmployeeController  : ControllerBase
 {
 
     [HttpPost]
+    [Route("employee")]
     public async Task<IActionResult> CreateEmployee(CreateEmployeeCommand createEmployeeCommand, ISender sender)
     {
         var result = await sender.Send(createEmployeeCommand);
--- src/Employee.API/Features/Employees/GetAllEmployees/GetEmployeeController.cs
+++ src/Employee.API/Features/Employees/GetAllEmployees/GetEmployeeController.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace Employee.API.Features.Employees.GetAllEmployees;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api")]
 public class GetEmployeeController : ControllerBase
 {
     
     [HttpGet]
+    [Route("employees")]
     public async Task<IActionResult> GetAllEmployees(ISender sender)
     {
         var query = new GetAllEmployeesQuery();
